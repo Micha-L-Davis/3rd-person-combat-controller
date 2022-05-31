e3275e147871a4bf9d73a54f9f8ddcfc8a5eb3b7
Micha Davis <78385236+Micha-L-Davis@users.noreply.github.com>
2022-05-31 11:27:57 -0700

    add input reader component

--- Assets/Scripts/State Machines/Player/PlayerStateMachine.cs
+++ Assets/Scripts/State Machines/Player/PlayerStateMachine.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerStateMachine : StateMachine
 {
+    [field: SerializeField] 
+    public InputReader InputReader { get; private set; }
+
     void Start()
     {
         
