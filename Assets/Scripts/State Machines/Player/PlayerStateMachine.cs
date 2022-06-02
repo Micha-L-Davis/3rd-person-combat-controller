86c427a4846f54b4e74f383873b01d768e4ba50d
Micha Davis <78385236+Micha-L-Davis@users.noreply.github.com>
2022-06-01 20:14:49 -0700

    added force receiver and gravity force effect, moved movement into Base State

--- Assets/Scripts/State Machines/Player/PlayerStateMachine.cs
+++ Assets/Scripts/State Machines/Player/PlayerStateMachine.cs
@@ -7,6 +7,8 @@ public class PlayerStateMachine : StateMachine
     [field: SerializeField] 
     public InputReader InputReader { get; private set; }
     [field: SerializeField]
+    public ForceReceiver ForceReceiver { get; private set; }
+    [field: SerializeField]
     public CharacterController Controller { get; private set; }
     [field: SerializeField]
     public TargetLocker TargetLocker { get; private set; }
@@ -18,7 +20,6 @@ public class PlayerStateMachine : StateMachine
     public float RotationSmoothing { get; private set; }
     public Transform MainCameraTransform { get; private set; }
     private bool isTargeting;
-
 
     void Start()
     {
